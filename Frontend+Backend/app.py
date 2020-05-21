79f089b8d1dabbb8e1d7c3a216db41e992014fec
ShaneyWaris <ShaneyWaris@users.noreply.github.com>
2020-05-21 22:08:34 +0530

    added flask files.

--- Frontend+Backend/app.py
+++ Frontend+Backend/app.py
@@ -1,28 +1,99 @@
-from flask import Flask,request,render_template
-import pickle
+from flask import Flask, render_template, request, url_for, redirect
+from flask_sqlalchemy import SQLAlchemy #We can rep our database structures as classes and you will be hearing those classes as MODELS.
+from F import LoginForm
 
 app = Flask(__name__)
 
+app.config['SECRET_KEY'] = '457486afbc06732795658e96ba989d1a'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+
+db = SQLAlchemy(app) #SQLAlchemy Database Instance Gets Created here. 
+
+class EMPLOYEE(db.Model):
+	USER_ID = db.Column(db.Integer, primary_key=True)
+	QUALIFICATION = db.Column(db.String(120), unique=False, nullable=False)
+	EXPERIENCE = db.Column(db.String(120), nullable=True)
+	# image_file = db.Column(db.String(120), nullable=False, nullable=False)
+	# password = db.Column(db.String(60), nullable=False)	
+
+	# def __repr__(self):
+	# 	return f"User('{}')"
+
+
+class EMPLOYER(db.Model):
+	USER_ID = db.Column(db.Integer, primary_key=True)
+	COMPANY_NAME = db.Column(db.String(120), unique=False, nullable=False)
+	COMPANY_SIZE = db.Column(db.Integer(20), nullable=False)
+	POSITION = db.Column(db.String(20), nullable=False)
+	SECTOR = db.Column(db.Integer(20), nullable=False)
+	SECTOR_TYPE = db.Column(db.Integer(20), nullable=False)
+	WEBSITE = db.Column(db.String(20), nullable=True)
+
+
+class USERS(db.Model):
+	USER_ID = db.Column(db.Integer, primary_key=True)
+	NAME = db.Column(db.String(20), unique=False, nullable=False)
+	EMAIL_ID = db.Column(db.String(20), nullable=False)
+	PASSWORD = db.Column(db.String(20), nullable=False)
+	USER_TYPE = db.Column(db.Integer(20), nullable=False)
+	PHONE_NUMBER = db.Column(db.String(20), nullable=False)
+
+
+class REQUIREMENT(db.Model):
+	USER_ID = db.Column(db.Integer, primary_key=True)
+	POSITION = db.Column(db.String(30), unique=False, nullable=False)
+	SKILL_REQ = db.Column(db.String(120), nullable=True)
+
+
+class SKILLS(db.Model):
+	USER_ID = db.Column(db.Integer, primary_key=True)
+	SKILL = db.Column(db.String(120), nullable=True)
+
+
 @app.route('/')
-def hello_world():
-    return render_template("login.html")
+@app.route('/home')
+def home():
+    return render_template('homePage.html')
 
-database={'nachi':'123','james':'aac','karthik':'asdsf'}
+@app.route('/about')
+def About():
+    return render_template('about.html', title='About')
 
-@app.route('/login',methods=['POST','GET'])
+@app.route('/login', methods=['POST', 'GET'])
 def login():
-    name1=request.form['Username']
-    pwd=request.form['Password']
-    print(name1)
-    print(pwd)
-    if name1 not in database:
-	    return render_template('login.html',info='Invalid User')
-    else:
-        return render_template('index.html')
+	form = LoginForm()
+	if request.method == 'POST':
+		user = request.form['nm']
+		return redirect(url_for('index', usr=index))
+	else:  
+		return render_template('login.html', title='login', form=form)
 
-@app.route('/index')
-def index():
-	return render_template('index.html')	
+@app.route("/userdashboard")
+def index(usr):
+	return render_template('index.html')
 
-if __name__ == '__main__':
-   	app.run()
+
+@app.route('/contact', methods=['POST', 'GET'])
+def Contact():
+	if request.method=='POST':
+		"""Add entry to the database here"""
+		Your Name = request.form.get('name')
+		Your Email = request.form.get('email')
+		Subject = request.form.get('subject')
+		Message = request.form.get('message')
+
+    return render_template('contact.html')
+
+
+# @app.route('/signup')
+# def SignUp():
+#     return render_template('SignUp.html')
+
+# @app.route('/register')
+# def register():
+#    form = RegistrationForm()
+#    return render_template('SignUp.html', title='SignUp', form=form)
+
+
+if '__name__' == '__main__':
+	app.run(debug=True) 
